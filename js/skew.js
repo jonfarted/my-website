f90605326d0dec00461ae8a61b1d408eb2b908fe
Jon <charlee.mcghee@gmail.com>
2018-04-11 09:58:48 -0700

    overlay and skew implemented
    So the overlay and skew on scroll have been implemented but still need refining. I'm still figuring out how to load images into a slide show based on the project that was clicked. The overlay is also supposed to have a horizontal scroll, but I need to implement horizontal scroll wheel functionality as well as make the skew.js work on horizontal scroll

--- js/skew.js
+++ js/skew.js
@@ -4,7 +4,7 @@ let currentPixel = window.pageYOffset
 const looper = function () {
   const newPixel = window.pageYOffset
   const diff = newPixel - currentPixel
-  const speed = diff * 1
+  const speed = diff * .75
 
   section.style.transform = "skewY(" + speed + "deg)"
 
