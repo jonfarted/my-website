6f914e1d6d9db1b4562b696f05ff62e808c0e613
Jon <charlee.mcghee@gmail.com>
2018-04-27 16:55:25 -0700

    almost there
    pick up in europe

--- js/scroller.js
+++ js/scroller.js
@@ -1,39 +1,8 @@
-document.getElementById('top')[0].onclick = function () {
- scrollTo(document.body, 0, 1250);
-}
-
-function scrollTo(element, to, duration) {
-  var start = element.scrollTop,
-      change = to - start,
-      currentTime = 0,
-      increment = 20;
-
-  var animateScroll = function(){
-      currentTime += increment;
-      var val = Math.easeInOutQuad(currentTime, start, change, duration);
-      element.scrollTop = val;
-      if(currentTime < duration) {
-          setTimeout(animateScroll, increment);
-      }
-  };
-  animateScroll();
-}
-
-//t = current time
-//b = start value
-//c = change in value
-//d = duration
-Math.easeInOutQuad = function (t, b, c, d) {
-t /= d/2;
-if (t < 1) return c/2*t*t + b;
-t--;
-return -c/2 * (t*(t-2) - 1) + b;
-};
-/*window.onload = preLoader;*/
-
-function preLoader() {
-  javascript:document.getElementById('loader').style.visibility="visible";
-  javascript:document.getElementById('loader').style.opacity=1;
-}
-
-setTimeout(function() { preLoader(); }, 300);
+function scrollToTop() {
+		var position = document.body.scrollTop || document.documentElement.scrollTop;
+		if (position){
+			window.scrollBy(0,-Math.max(10, Math.floor(position / 10)));
+			scrollAnimation=setTimeout('scrollToTop()',10);
+		}
+		else clearTimeout(scrollAnimation);
+	}
